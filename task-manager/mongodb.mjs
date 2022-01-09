f3524bbb8461c1898ba7cef944b1c0fe66a71496
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-09 16:49:10 +0200

    Add Many

--- task-manager/mongodb.mjs
+++ task-manager/mongodb.mjs
@@ -1,4 +1,19 @@
 import { MongoClient } from 'mongodb'
+// DATA
+const many = [{
+	name: 'Mansour Koura',
+	age: 22,
+}, {
+	name: 'Mohamed Abd-elmoniem',
+	age: 23,
+}, {
+	name: 'Amr Ahmed',
+	age: 23,
+}, {
+	name: 'Khalid Ahmed',
+	age: 24,
+}];
+
 
 // Connection URL
 const url = 'mongodb://localhost:27017';
@@ -15,14 +30,18 @@ async function main() {
 
 	const db = client.db(dbName);
 	const usersCollection = db.collection('users');
+	const tasksCollection = db.collection('tasks');
 
 	// Insert a Document
-	// const insertResult = 
 	await usersCollection.insertOne({
 		name: 'Mansour Koura',
 		age: 22,
 	}).then(({ insertedId }) => console.log(insertedId))
-	// console.log(insertResult);
+		.catch('Unable to insert Doc!')
+
+	await usersCollection.insertMany(many).then(({ insertedCount, insertedIds }) => console.log(`Insrted users num: ${insertedCount}`, insertedIds))
+		.catch('Unable to insert Doc!')
+
 
 	// // Find All Documents
 	// const findResult = await usersCollection.find({}).toArray();
@@ -32,11 +51,10 @@ async function main() {
 	// const updateResult = await usersCollection.updateOne({ a: 3 }, { $set: { b: 1 } });
 	// console.log('Updated documents =>', updateResult);
 
-
 	// // Remove a document
 	// const deleteResult = await usersCollection.deleteMany({ a: 3 });
 	// console.log('Deleted documents =>', deleteResult);
-	
+
 	// // Index a Collection
 	// const indexName = await usersCollection.createIndex({ a: 1 });
 	// console.log('index name =', indexName);
