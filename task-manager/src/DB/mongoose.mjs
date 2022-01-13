8393dc71208f970a8b816b7fc10060024dbea02a
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-13 23:13:59 +0200

    Update mongoose

--- task-manager/src/DB/mongoose.mjs
+++ task-manager/src/DB/mongoose.mjs
@@ -3,17 +3,19 @@ import mongoose from 'mongoose';
 async function main() {
 	await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
 
-const User = new mongoose.Schema({
-	name: String,
-	age: Number,
-})
+	const UserSchema = new mongoose.Schema({
+		name: String,
+		age: Number,
+	});
+
+	const addUser = mongoose.model('addUser', UserSchema);
+	const Mansour = new addUser({ name: 'Mansour Koora', age: 22 })
+	await Mansour.save()
+		.then(console.log(Mansour))
+		.catch(console.error)
+
 }
-const Kitten = mongoose.model('Kitten', kittySchema);
-
-const Mansour = new User({name: 'Mansour Koora'})
-
-await Mansour.save()
 
 main()
-	.then(console.log('Hi'))
+	.then(console.log)
 	.catch(console.error)
