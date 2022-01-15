b3cdf7809e4c6f471928b59b97e2bc1c1751db04
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-15 22:30:37 +0200

    Tasks endpoint setup

--- task-manager/src/DB/mongoose.mjs
+++ task-manager/src/DB/mongoose.mjs
@@ -1,26 +1,6 @@
 import mongoose from 'mongoose';
 
-async function connectDB() {
+export default async function connectDB() {
 	await mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
-
-	// Instances of the models
-	// const user = new User({
-	// 	name: 'Basha',
-	// 	email: 'mans@mans.mans',
-	// 	age: 1,
-	// 	password: '           Pass,,word       ',
-	// })
-	// const task = new Task({
-	// 	description: 'Done Node till 30 => 101',
-	// 	completed: false,
-	// })
-
-
-	// 	await task.save()
-	// 		.then(console.log(task))
-	// 		.catch(console.error)
 }
 
-export default connectDB()
-	.catch(console.error)
-	.finally(() => mongoose.disconnect());
--- task-manager/src/index.mjs
+++ task-manager/src/index.mjs
@@ -4,20 +4,34 @@ import connectDB from './DB/mongoose.mjs';
 import User from './Models/user.mjs'
 import Task from './Models/task.mjs'
 
-
-connectDB()
-
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json())
 
+connectDB()
+
 app.post('/users', (req, res) => {
 	const user = new User(req.body)
-
 	user.save()
-		.then(res.send(user))
-		.catch(console.error)
+		.then(() => {
+			res.send(user)
+		})
+		.catch((error) => {
+		res.send(error).status(400)
+		})
+})
+
+app.post('/tasks', (req, res) => {
+	const task = new Task(req.body)
+	task.save()
+	.then(() => {
+		res.send(task)
+
+	})
+	.catch((error) => {
+		res.send(error).status(400)
+	})
 })
 
 app.listen(port, () => {
