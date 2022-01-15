9494b852aff8da50d487809f7f87884117496fc6
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-15 20:31:30 +0200

    API End points

--- task-manager/src/index.mjs
+++ task-manager/src/index.mjs
@@ -3,7 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json())
+
 app.post('/users', (req, res) => {
+	console.log(req.body);
 	res.send('testing ya m3lm 🧪')
 })
 
