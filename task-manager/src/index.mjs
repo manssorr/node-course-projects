fc441b63dd7af33a74a72dd00453ee852897ad56
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-17 12:17:44 +0200

    Express meddle-wares

--- task-manager/src/index.mjs
+++ task-manager/src/index.mjs
@@ -3,9 +3,20 @@ import connectDB from './db/mongoose.mjs';
 import usersRouter from './routers/user.mjs';
 import tasksRouter from './routers/task.mjs';
 
-
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use((req, res, next) => {
+	if(req.method === 'GET') {
+		res.send('GET requests are desibled 😛');
+	} else {
+		next()
+	}
+})
+
+app.use((req, res, next) => {
+	res.status(503).send('قافلين النهارده يا عمنا ✨');
+})
 
 app.use(express.json());
 app.use(usersRouter)
--- task-manager/src/routers/user.mjs
+++ task-manager/src/routers/user.mjs
@@ -8,7 +8,8 @@ router.post('/users', async (req, res) => {
 	const user = new User(req.body)
 	try {
 		await user.save()
-		res.status(201).send(user)
+		const token = user.generateAuthToken()
+		res.status(201).send({ user, token })
 	} catch (error) {
 		res.status(400).send(error.message)
 	}
