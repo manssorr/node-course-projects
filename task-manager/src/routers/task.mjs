11b735a8617e1afb4991b0645e61167b8ed94320
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-23 00:23:41 +0200

    Tasks Update

--- task-manager/src/routers/task.mjs
+++ task-manager/src/routers/task.mjs
@@ -8,7 +8,7 @@ router.post('/tasks', auth, async (req, res) => {
 	const task = new Task({
 		...req.body,
 		owner: req.user._id,
-	})
+	}) 
 	try {
 		await task.save()
 		res.status(201).send(task)
