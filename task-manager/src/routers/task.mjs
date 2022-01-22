8bece1767ef316c83d29854154dd5d2bf2210e7c
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-22 11:29:02 +0200

    Update Task

--- task-manager/src/routers/task.mjs
+++ task-manager/src/routers/task.mjs
@@ -7,7 +7,7 @@ const router = express.Router()
 router.post('/tasks', auth, async (req, res) => {
 	const task = new Task({
 		...req.body,
-		Owner: req.user._id,
+		owner: req.user._id,
 	})
 	try {
 		await task.save()
@@ -36,7 +36,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 		res.status(500).send()
 	}
 });
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
 	const updates = Object.keys(req.body)
 	const allowedUpdates = ['description', 'completed']
 	const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -59,10 +59,10 @@ router.patch('/tasks/:id', async (req, res) => {
 		res.status(400).send(error)
 	}
 })
-router.delete('/tasks/:id', async (req, res) => {
+router.delete('/tasks/:id', auth, async (req, res) => {
 	const _id = req.params.id
 	try {
-		const task = await Task.findByIdAndDelete(_id);
+		const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
 
 		if (!task) {
 			return res.status(404).send()
