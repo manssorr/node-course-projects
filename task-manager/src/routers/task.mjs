6178a58dfba6de43c4e3afa941d9971f80292081
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-22 11:15:42 +0200

    Update Task

--- task-manager/src/routers/task.mjs
+++ task-manager/src/routers/task.mjs
@@ -24,11 +24,10 @@ router.get('/tasks', async (req, res) => {
 		res.status(500).send()
 	}
 });
-router.get('/tasks/:id', async (req, res) => {
-	const _id = req.params.id
-	console.log(_id)
+router.get('/tasks/:id', auth, async (req, res) => {
+	// const _id = req.params.id
 	try {
-		const task = await Task.findById({ _id })
+		const task = await Task.findOne({ _id, owner: req.user._id })
 		if (!task) {
 			return res.status(404).send(task)
 		}
