30258c54664643c5e0c1981dc4faa6494903bcfe
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-23 17:32:54 +0200

    Update Tasks options

--- task-manager/src/routers/task.mjs
+++ task-manager/src/routers/task.mjs
@@ -30,7 +30,7 @@ router.get('/tasks', auth, async (req, res) => {
 
 	if (req.query.sortBy) {
 		const parts = req.query.sortBy.split(':')
-
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
 
 		try {
 			await req.user.populate({
