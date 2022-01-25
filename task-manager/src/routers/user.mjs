bf4ca3b39de11e0423e607f642a8051120dff445
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-26 01:12:01 +0200

    Update Mai lSender

--- task-manager/src/routers/user.mjs
+++ task-manager/src/routers/user.mjs
@@ -98,6 +98,7 @@ router.patch('/users/me', auth, async (req, res) => {
 router.delete('/users/me', auth, async (req, res) => {
 	try {
 		await req.user.remove();
+		sendCancelationEmail(req.user.email, req.user.name);
 		res.send(req.user);
 	} catch (error) {
 		res.status(500).send();
