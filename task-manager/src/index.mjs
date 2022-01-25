97a3b1f07fc8c8a2198766d0662052076f2f50df
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-25 15:01:20 +0200

    ِAdd Sorting And Paganation

--- task-manager/src/index.mjs
+++ task-manager/src/index.mjs
@@ -31,3 +31,18 @@ connectDB();
 app.listen(port, () => {
 	console.log(`Port: ${port} now Is OnFire 🔥`)
 }); 
+
+import Task from './models/user.js';
+import User from './models/task.js';
+
+const main = async () => {
+    // const task = await Task.findById('5c2e505a3253e18a43e612e6')
+    // await task.populate('owner').execPopulate()
+    // console.log(task.owner)
+
+    // const user = await User.findById('5c2e4dcb5eac678a23725b5b')
+    // await user.populate('tasks').execPopulate()
+    // console.log(user.tasks)
+}
+
+main()
