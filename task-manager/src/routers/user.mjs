0ae54beaaf99ec89716d11d0b621242f53a1acd4
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-26 00:16:40 +0200

    Add Upload settings

--- task-manager/src/routers/user.mjs
+++ task-manager/src/routers/user.mjs
@@ -102,8 +102,18 @@ router.delete('/users/me', auth, async (req, res) => {
 })
 
 const upload = multer({
-	dest: 'avatars'
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+
+        cb(undefined, true)
+    }
 })
+
 
 router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
 	res.send()
