d5a4e1a3d570b540cc75ab2d6066f7870427b05f
Mansour Zorin <mansourashraf173@gmail.com>
2022-01-26 01:32:12 +0200

    Update ENVs

--- task-manager/src/middleware/auth.mjs
+++ task-manager/src/middleware/auth.mjs
@@ -5,7 +5,7 @@ export default async (req, res, next) => {
 	console.log("Hi! 👋, I'm Auth 🗝️ ");
 	try {
 		const token = req.header('Authorization').replace('Bearer ', '');
-		const decoded = jwt.verify(token, 'Alhamdullah');
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
 		const user = await User.findOne({ _id: decoded._id, "tokens.token": token })
 
 		console.log('AlSalamAlykom man 👋 !');
